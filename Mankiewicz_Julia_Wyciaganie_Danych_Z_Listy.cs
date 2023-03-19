022c6c8ec8daad53d6da022da3fb1ef081a9d031
mankiewicx <116458917+mankiewicx@users.noreply.github.com>
2023-03-19 19:14:35 +0100

    Add files via upload

---
+++ Mankiewicz_Julia_Wyciaganie_Danych_Z_Listy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace List.ConsoleApp
+{
+    class List
+    {
+        public static void Main(string[] args)
+        {
+            string list = System.IO.File.ReadAllText("./../../../NameList.txt");
+            string[] arrtext = list.Split("\n");
+
+            Console.WriteLine("Imię i Nazwisko 246 osoby z listy to: " + arrtext[246] + "\n");
+            foreach (var name in arrtext)
+            {
+                if (name.Contains("Anna"))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
+
